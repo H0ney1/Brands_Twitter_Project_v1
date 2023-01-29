ae7b2c0493aab42ce44ff3f4824aeff3d1870de2
Slava <deniskindenisk@gmail.com>
2023-01-29 14:37:57 -0600

    Adding the files to share

---
+++ TWManagement_functions.py
@@ -0,0 +1,78 @@
+import datetime
+import sqlite3
+import os
+import pandas as pd
+
+# Get the Start and End Datetimes function
+def CollectTimePoints(filename):
+    # Get the file open with current start time
+    with open(filename,'r') as file:
+        start_time = file.read()
+    # Set the current time as end_time
+    end_time = datetime.datetime.now()
+    # Format End Time
+    end_time = end_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+    # Return the variables
+    return start_time, end_time
+
+# Save New Start time
+def SetNewStartTime(filename,end_time):
+    with open(filename,'w') as file:
+        file.write(str(end_time))
+
+# Get Tweet IDs from the table 
+def CollectPastTweets(db_path):
+    # Check if database exists
+    if not os.path.exists(db_path):
+        return []
+    # Get the unique tweets
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    # Query the tweets
+    c.execute("SELECT DISTINCT Tweet_ID FROM Tweets_Metrics")
+    tweet_ids = [row[0] for row in c.fetchall()]
+    # Close connection
+    c.close()
+    conn.close()
+
+    return tweet_ids
+
+# Put Past Tweet IDs into workable form - list of lists
+def SplitPastTweets(string_list, max_size = 100):
+    return [string_list[i:i+max_size] for i in range(0,len(string_list))]
+
+# Merge Old tweet metrics with new for the time period
+def MergeTweetMetrics(TweetMetricsNew,TweetMetricsPast):
+    result = pd.concat([TweetMetricsNew, TweetMetricsPast])
+    return result
+
+def CorrectFormat(df):
+    # Define a list of column names to be fetched
+    columns_to_fetch = ["Tweet_ID","Mentions_ID","Hashtags","URLs","Attachments_Type","Image_URLs","Video_URLs"]
+    columns_to_keep = ["Date","Date_Created","Brand_ID","Tweet_ID","Reply_To",
+    "Text","Mentions_Count","Hashtags_Count","URLs_Count","Attachments_Count","Conversation_ID"]
+    # Two dataframes - one for content, one for count
+    dfAttContent = df[columns_to_fetch]
+    dfAttCount = df[columns_to_keep]
+    # Get Attachment Content into correct format
+    for c in columns_to_fetch[1:]:
+        dfAttContent = dfAttContent.explode(c)
+    
+    dfAttContent = pd.melt(dfAttContent,id_vars = "Tweet_ID",value_vars = columns_to_fetch[1:])
+    dfAttContent = dfAttContent.rename(columns={'variable':'Additional_Type','value':'Additional_Values'})
+
+    return dfAttCount,dfAttContent
+    
+
+# Push the dataframes as tables to the database:
+def AddScrapedData(dfUsers, dfTweetMetrics, dfTweetAtts,dfTweetAttsContent, db_path):
+    # Set Connection
+    conn = sqlite3.connect(db_path)
+    # Append each df
+    dfUsers.to_sql('Brand_Accounts', conn, if_exists = "append")
+    dfTweetMetrics.to_sql('Tweets_Metrics', conn, if_exists = "append")
+    dfTweetAtts.to_sql('Tweets_Attributes', conn, if_exists = "append")
+    dfTweetAttsContent.to_sql('Tweets_Attributes_Detailed', conn, if_exists = "append")
+    # Close connection
+    conn.commit()
+    conn.close()
---
+++ TWScrape_Script.py
@@ -0,0 +1,73 @@
+import tweepy
+from pathlib import Path
+from Scrape_functions import *
+from Management_functions import *
+
+######## PATH + API KEYS ############
+# Set path to the database
+api_keys_path = Path("YOUR PATH TO API KEYS FILE(S) HERE")
+directory_path = Path("PATH TO FOLDER WITH THE DATABASE")
+db_path = directory_path/"DB_NAME_HERE.db"
+
+######## SET API CREDENTIALS - I STORE EACH AS SEPARATE .TXT FILE#####
+# API Credentials File Names
+filenames = ["API.txt","APIsecret.txt","bToken.txt"]
+
+# Get API Credentials
+Creds = {}
+for file in filenames:
+    key = file.split('.')[0]
+    with open(api_keys_path/file, "r") as f:
+        value = f.read()
+    Creds[key] = value
+
+###############################################################################
+## NOTE: set up your client as "client", and keep wait on rate limit as TRUE ##
+###############################################################################
+
+# Create the client to query the API with
+client = tweepy.Client(bearer_token = Creds["bToken"],consumer_key = Creds["API"],
+consumer_secret = Creds["APIsecret"], wait_on_rate_limit = True)
+
+# Get Input Data Ready
+start_date, end_date = CollectTimePoints(filename = directory_path/"start_time.txt")
+brand_ids = CollectInputIDs(filename = directory_path/"id_list.txt")
+print("Total ids Acquired: " + str(len(brand_ids)))
+
+# Scrape User Data
+print("Starting to Scrape Data")
+dfUsers, dfErrorUsers = CollectUserData(user_ids = brand_ids, date = end_date)
+
+# Account for any problems
+print("Checking for Errors")
+FlagProblemIDs(ProblemIDs = dfErrorUsers, filename = directory_path/"ProblemIDs.csv", end_date = end_date)
+
+# Get New Tweets
+print("Starting to get data for tweets")
+TweetMetricsNew, dfTweetAtts = GetNewTweets(user_ids = brand_ids, start_date = start_date, end_date = end_date)
+
+# Get the IDs for past tweets
+print("Updating metrics for existing tweets")
+past_tweet_ids = CollectPastTweets(db_path = db_path)
+past_tweets_grouped = SplitPastTweets(string_list = past_tweet_ids)
+print("Total IDs to update: ", len(past_tweet_ids))
+print("Queries to push: " + str(len(past_tweets_grouped)))
+
+# Get Info on past tweets
+TweetMetricsPast = CollectPastTweetsMetrics(past_tweets_grouped)
+print("Metrics updated")
+
+# Merge with Current tweets metrics
+print("Preparing data for push to database")
+dfTweetMetrics = MergeTweetMetrics(TweetMetricsNew, TweetMetricsPast)
+
+# Correct Format of Columns
+dfTweetAttachments, dfTweetAttContent = CorrectFormat(df = dfTweetAtts)
+
+# Push these to the dataframe
+print("Adding new data to database")
+AddScrapedData(dfUsers,dfTweetMetrics,dfTweetAttachments,dfTweetAttContent,db_path)
+
+#Change the time
+print("Data added, setting new start time for next run")
+SetNewStartTime(filename = directory_path/"start_time.txt",end_time = end_date)
